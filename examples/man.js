13772e4ee958323c9aefe7f153682f43012db26c
Tal <anlv.tan@tendcloud.com>
2015-03-17 17:09:59 +0800

    list wechat callback server ip.

--- examples/man.js
+++ examples/man.js
@@ -2,7 +2,9 @@ var co = require('co'),
     api = new (require('../').Api)('YOUR APPID', 'YOU APP SECRET');
 
 co(function *() {
-    console.log(yield api.getAccessToken());
+    var accessToken = (yield* api.getAccessToken()).access_token;
+    console.log(accessToken);
+    console.log(yield* api.listIp(accessToken));
 }).catch(function (err) {
     console.log(err.stack || err);
 });
